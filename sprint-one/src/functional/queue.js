b412f29f36d2aa58f731d5ca8ec4ad663b7ec15c
gladysbenitez <gladysbenitez.an@gmail.com>
2019-04-11 11:34:05 -0500

    working solution for functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,55 +1,36 @@
 var Queue = function() {
   var someInstance = {};
-
-
-  // Use an object with numeric keys to store values
   someInstance.storage = {};
-  // someInstance.storage.head = null;
-  // someInstance.storage.tail = null;
   someInstance.i = 0; 
-  //track index and counter;////////////////////////////////////////////////////
+  someInstance.counter = 0;
+  someInstance.sz = 0;
 
-  // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    someInstance.storage[someInstance.i]= value;
-    someInstance.storage
+    someInstance.storage[someInstance.i] = value;
     someInstance.i++;
+    someInstance.sz++;
   };
 
   someInstance.dequeue = function() {
-    var poppedItem = someInstance.storage[0]
+    var poppedItem = someInstance.storage[someInstance.counter];
+    delete someInstance.storage[someInstance.counter]; 
 
-     console.log( ' someInstance.storage[0]')
-    delete someInstance.storage[0]; 
-    
-    someInstance.i--;
-    console.log('popped item', poppedItem)
-    return poppedItem
-    
+    someInstance.counter++;
+    someInstance.sz--;
+
+    return poppedItem;
   };
 
   someInstance.size = function() {
-    if (someInstance.i >= 0 ){
-       return someInstance.i
-    } else{ 
-      someInstance.i = 0
+    if (someInstance.sz >= 0) {
+      return someInstance.sz;
+    } else { 
+      someInstance.sz = 0;
     }
-    return someInstance.i;
-  };
+    return someInstance.sz;
+  }; 
 
-  console.log(" is someInstance", someInstance.storage)
   return someInstance;
 };
 
-
-
-// delete someInstance.storage[0]; 
-// if (someInstance.i > Object.keys(someInstance.storage).length){
-//   someInstance.i = 0
-// }
-// for(var key in someInstance.storage){
-//   key = JSON.stringify(parseInt(key)-1);
-// }
-// someInstance.i--;
-//  return(someInstance.storage[someInstance.i])
