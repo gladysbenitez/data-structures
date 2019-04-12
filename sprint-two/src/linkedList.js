a89a9ba28e230708ffb0d4916a6ebb45e01aa3bd
gladysbenitez <gladysbenitez.an@gmail.com>
2019-04-12 09:46:21 -0500

    complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);  
+    if(!list.tail) {
+      list.head = newNode; 
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var removedItem = list.head.value;
+    if(!list.head) {
+      return null;
+    }
+    list.head = list.head.next;
+    return removedItem;
   };
 
   list.contains = function(target) {
-  };
-
+    var currentNode = list.head; 
+    while(currentNode) {
+      if(currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    } 
+    return false;
+ };
   return list;
 };
 
